b573a0ae3a16e4b64db9572bff6eba2ebcb23652
hwagyesa <kingofbads@gmail.com>
2024-10-14 16:08:44 -0500

    lenet

--- cifar10-classify/main.py
+++ cifar10-classify/main.py
@@ -6,6 +6,7 @@ import torch
 from omegaconf import DictConfig, OmegaConf
 from torch import nn
 from torch.utils.data import DataLoader
+from torchvision import transforms
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import ToTensor
 
@@ -16,12 +17,27 @@ import wandb
 def train(cfg: DictConfig):
     # config and logging
     config_dict = OmegaConf.to_container(cfg, resolve=True)
-    wandb.init(project="test_train_cifar10", name="resnet18", config=config_dict)
+    wandb.init(project="test_train_cifar10", name="lenet", config=config_dict)
     device = cfg.training.device
 
     # data
-    train_data = CIFAR10(root="data/", train=True, transform=ToTensor(), download=True)
-    val_data = CIFAR10(root="data/", train=False, transform=ToTensor(), download=True)
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616]
+    )
+    transform = transforms.Compose(
+        [
+            transforms.RandomResizedCrop(32),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+    transform_val = transforms.Compose([transforms.ToTensor(), normalize])
+
+    train_data = CIFAR10(root="data/", train=True, transform=transform, download=True)
+    val_data = CIFAR10(
+        root="data/", train=False, transform=transform_val, download=True
+    )
     train_loader = DataLoader(
         train_data,
         batch_size=cfg.training.batch_size,
@@ -39,24 +55,72 @@ def train(cfg: DictConfig):
     # model
     # CIFAR10 is 3 x 32 x 32 images.
     # There are 10 classes.
-    model = nn.Sequential(nn.Flatten(), nn.Linear(3 * 32 * 32, 10))
+
+    # A linear model, to get things rolling
+    # model = nn.Sequential(nn.Flatten(), nn.Linear(3 * 32 * 32, 10))
+
+    # A simple convolutional model, like LeNet.
+    # Follow https://github.com/soapisnotfat/pytorch-cifar10/blob/master/models/LeNet.py
+    model = nn.Sequential(
+        nn.Conv2d(3, 6, kernel_size=5),  # 32 - 5 + 1 = 28
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2),  # 28 / 2 = 14
+        nn.Conv2d(6, 16, kernel_size=5),  # 14-5+1 = 10
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2),  # 10 / 2 = 5. now at 16x5x5
+        nn.Flatten(),
+        nn.Linear(16 * 5 * 5, 120),
+        nn.ReLU(),
+        nn.Linear(120, 84),
+        nn.ReLU(),
+        nn.Linear(84, 10),
+    )
 
     # loss, optim, train loop
     loss_fn = nn.CrossEntropyLoss(reduction="mean")
     optimizer = torch.optim.AdamW(
         model.parameters(),
-        lr=cfg.training.learning_rate,
+        lr=cfg.training.lr_initial,
         weight_decay=cfg.training.weight_decay,
     )
+    # For doing LR scheduling:
+    # 1. We create a scheduler object that wraps the optimizer.
+    # 2. We optimize as usual, but we step the scheduler at certain key epochs (outer loop).
+    #    The scheduler will be initialized with certain schedule parameters. Step takes the epoch as argument.
+    #    (oops, this is depracated)
+    # General intuition about scheduling:
+    # A) start large. if initial training is unstable, consider (linear) warmup phase, going up to the initial large LR.
+    # B) decay the LR over training with the scheduler.
+    # C) for vision, consider using restarting on the LR decay
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=cfg.training.epochs
+    )
+
+    model.to(device)
     for epoch in range(cfg.training.epochs):
-        for X, Y in train_loader:
+        model.train()
+        for batch, (X, Y) in enumerate(train_loader):
             # do work
+            X = X.to(device, non_blocking=True)
+            Y = Y.to(device, non_blocking=True)
             loss, acc = process_batch(X, Y, loss_fn, model, optimizer, training=True)
-            wandb.log({"batch_loss_train": loss, "batch_acc_train": acc})
+            wandb.log(
+                {"batch": batch, "batch_loss_train": loss, "batch_acc_train": acc}
+            )
+        # Step the scheduler
+        scheduler.step()
         # Compute val loss on epoch
-        for X, Y in val_loader:
-            loss, acc = process_batch(X, Y, loss_fn, model, optimizer, training=False)
-            wandb.log({"epoch_loss_val": loss, "epoch_acc_val": acc})
+        model.eval()
+        with torch.no_grad():
+            for X, Y in val_loader:
+                X = X.to(device, non_blocking=True)
+                Y = Y.to(device, non_blocking=True)
+                loss, acc = process_batch(
+                    X, Y, loss_fn, model, optimizer, training=False
+                )
+                wandb.log(
+                    {"epoch": epoch, "epoch_loss_val": loss, "epoch_acc_val": acc}
+                )
 
     # eval and logging
     wandb.finish()
